877f794e5ab710d0dbfbbea3edc11e0d05b5c15d
CodeDad <github email address>
2018-07-14 16:44:36 -0400

    addend an artist column to the albums db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180623012953) do
+ActiveRecord::Schema.define(version: 20180714204327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180623012953) do
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "artist"
   end
 
   create_table "artists", force: :cascade do |t|
